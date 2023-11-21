79d1ce052230a533ff9e7a6bf65291d4a75f2bd7
saa00123 <saa00123@naver.com>
2023-11-21 15:32:49 +0900

    fix: db fix

--- todolist/resources/js/app.js
+++ todolist/resources/js/app.js
@@ -1,10 +1,6 @@
 import { createApp } from "vue";
 import TodoList from "./components/TodoList.vue";
 
-const app = createApp({
-    components: {
-        "todo-list": TodoList,
-    },
-});
-
+const app = createApp({});
+app.component("todo-list", TodoList);
 app.mount("#app");
--- todolist/vite.config.js
+++ todolist/vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-    plugins: [vue(), laravel(["resources/css/app.css", "resources/js/app.js"])],
+    plugins: [
+        laravel({
+            input: "resources/js/app.js",
+            refresh: true,
+        }),
+        vue(),
+    ],
     resolve: {
         alias: {
             vue: "vue/dist/vue.esm-bundler.js",
