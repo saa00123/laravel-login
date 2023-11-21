bff083a4adbeee6f330158c18bf1a5eb7f3a3c60
saa00123 <saa00123@naver.com>
2023-11-21 10:48:22 +0900

    feat: login authcontroller

---
+++ login/app/Http/Controllers/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Login;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'nickname' => 'required|unique:login|max:255',
+            'password' => 'required|min:6',
+            'email' => 'required|email|unique:login|max:255',
+            'phone' => 'nullable|max:255'
+        ]);
+
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        $login = Login::create($validatedData);
+
+        return response()->json(['message' => 'User registered successfully', 'login' => $login]);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'nickname' => 'required',
+            'password' => 'required'
+        ]);
+
+        $login = Login::where('nickname', $credentials['nickname'])->first();
+
+        if (!$login || !Hash::check($credentials['password'], $login->password)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        Session::put('login', $login->id);
+
+        return response()->json(['message' => 'Logged in successfully']);
+    }
+}
